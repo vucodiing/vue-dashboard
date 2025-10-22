bf6f7f04b05bc3d0e7d97b82be980f99e585e06b
LongVux <vucodiing@gmail.com>
2025-10-22 15:23:47 +0700

    add: format code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
-console.log('dfsdf')
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
