3ed1597b3616bf28a08a406bfde6eaf157a80039
LongVux <vucodiing@gmail.com>
2025-10-22 10:57:32 +0700

    test husky

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <p>Trang web</p>
 </template>
 
 <style scoped></style>
