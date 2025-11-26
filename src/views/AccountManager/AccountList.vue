0e8142b6d365ca7af8abed8f3ff5cca077931d53
LongVux <vucodiing@gmail.com>
2025-11-26 16:14:44 +0700

    update: css tài khoản

--- src/views/AccountManager/AccountList.vue
+++ src/views/AccountManager/AccountList.vue
@@ -13,15 +13,29 @@
           <el-table-column label="Vai trò" width="250">
             <template #default="scope">
               <div class="role-container">
-                <template v-for="role in scope.row.roles" :key="role">
-                  <el-tag v-if="role !== 'User'" :type="getRoleColor(role)" class="role-tag">
-                    {{ getRoleName(role) }}
-                  </el-tag>
-                </template>
+                <span
+                  v-for="role in filterRoles(scope.row.roles)"
+                  :key="role"
+                  class="role"
+                  :class="{
+                    admin: role === 'Admin',
+                    commune: role === 'Commune',
+                    'commune-agent': role === 'CommuneAgent',
+                  }"
+                >
+                  <i v-if="role === 'Admin'" class="fa-solid fa-clipboard-list"></i>
+                  <i v-if="role === 'Commune'" class="fa-solid fa-nfc"></i>
+                  <i v-if="role === 'CommuneAgent'" class="fa-solid fa-head-side-headphones"></i>
+                  {{ role }}
+                </span>
               </div>
             </template>
           </el-table-column>
-          <el-table-column prop="disabled" label="Trạng thái" />
+          <el-table-column prop="disabled" label="Hoạt động">
+            <template #default="scope">
+              <el-switch class="ml-2" :model-value="!scope.row.disabled" />
+            </template>
+          </el-table-column>
         </el-table>
       </div>
       <div class="v-pagination">
@@ -49,6 +63,13 @@ import { onMounted, ref } from 'vue';
 import mushroom from '@/service/api/mushroom-api';
 import dataForm from './dataForm';
 import method from '@/utils/method';
+
+const filterRoles = (roles: (string | null | undefined)[] | null | undefined) => {
+  if (!roles) return [];
+  return roles.filter(
+    (role): role is string => role !== 'User' && role !== null && role !== undefined,
+  );
+};
 
 const tableData = ref<User[]>([]);
 const tableRules = dataForm.tableRules;
@@ -117,3 +138,34 @@ onMounted(async () => {
   await fetchUserList();
 });
 </script>
+
+<style scoped lang="scss">
+.role-container {
+  display: flex;
+  gap: 8px;
+}
+
+.role {
+  padding: 2px 8px;
+  border-radius: var(--border-radius-md);
+  font-size: 13px;
+  i {
+    margin-right: 4px;
+  }
+}
+
+.admin {
+  background-color: rgba(237, 36, 56, 0.2);
+  color: var(--color-primary);
+}
+
+.commune {
+  background-color: rgba(15, 124, 207, 0.2);
+  color: var(--color-secondary);
+}
+
+.commune-agent {
+  background-color: rgba(34, 197, 94, 0.2);
+  color: var(--color-success);
+}
+</style>
